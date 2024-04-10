9c153d749e2614ab736ca88bb9ed1ea2ba1e01d4
Pablo Soares Teixeira <pablopst@gmail.com>
2024-04-10 08:14:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const axios = require("axios");
-
-const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 34160;
-const SELL_PRICE = 34501;
+const crypto = require("crypto");
 
 const API_URL = "https://testnet.binance.vision";//https://api.binance.com
+const SYMBOL = "BTCUSDT";
+const BUY_PRICE = 100000;
+const SELL_PRICE = 100000;
+const QUANTITY = "0.001";
+const API_KEY = "XXX";//aprenda a criar as chaves: https://www.youtube.com/watch?v=-6bF6a6ecIs
+const SECRET_KEY = "XXX";
 
 let isOpened = false;
 
-
-function calcSMA(data){
+function calcSMA(data) {
     const closes = data.map(candle => parseFloat(candle[4]));
     const sum = closes.reduce((a, b) => a + b);
     return sum / data.length;
@@ -23,22 +25,45 @@ async function start() {
     console.clear();
     console.log("Price: " + price);
 
-    const sma21 = calcSMA(data);
-    const sma13 = calcSMA(data.slice(8));
+    const sma = calcSMA(data);
+    console.log("SMA: " + sma);
+    console.log("Is Opened? " + isOpened);
 
-    console.log('sma21: ', sma21);
-    console.log('sma13: ', sma13);
-
-    if(sma13 > sma21 && isOpened === false){
-        console.log("comprar");
+    if (price < (sma * 0.9) && isOpened === false) {
         isOpened = true;
+        newOrder(SYMBOL, QUANTITY, "BUY");
     }
-    else if(sma13 < sma21 && isOpened === true){
-        console.log("vender");
+    else if (price > (sma * 1.1) && isOpened === true) {
+        newOrder(SYMBOL, QUANTITY, "SELL");
         isOpened = false;
     }
-    else
-        console.log("aguardar");
+}
+
+async function newOrder(symbol, quantity, side) {
+    const order = { symbol, quantity, side };
+    order.type = "MARKET";
+    order.timestamp = Date.now();
+
+    const signature = crypto
+        .createHmac("sha256", SECRET_KEY)
+        .update(new URLSearchParams(order).toString())
+        .digest("hex");
+
+    order.signature = signature;
+
+    try {
+        const { data } = await axios.post(
+            API_URL + "/api/v3/order",
+            new URLSearchParams(order).toString(),
+            {
+                headers: { "X-MBX-APIKEY": API_KEY }
+            });
+
+        console.log(data);
+    } catch (err) {
+        //para erros e soluções com essa API, consulte https://www.luiztools.com.br/post/erros-comuns-com-as-apis-da-binance/
+        console.error(err.response.data);
+    }
 }
 
 setInterval(start, 3000);
